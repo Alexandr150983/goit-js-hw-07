e282cf2d0d8fe8d74a7d65bd7f7d5bcc8813ddf5
Alexandr150983 <alexandr150983@gmail.com>
2023-07-20 14:33:25 +0300

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,76 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+let instance = null;
+
+function createGalleryItemMarkup({preview, original, description}) {
+  return `
+  <li class="gallery__item">
+  <a class="gallery__link" href='${original}'>
+    <img
+      class="gallery__image"
+      src='${preview}'
+      data-source='${original}'
+      alt='${description}'
+    />
+  </a>
+</li>`;
+}
+
+function renderGallery() {
+  const galleryMarkup = galleryItems.map((item) => createGalleryItemMarkup(item)).join('');
+  
+  galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+}
+
+renderGallery();
+
+galleryContainer.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  if (event.target.classList.contains('gallery__image')) {
+    const imageUrl = event.target.dataset.source;
+    instance = basicLightbox.create(`<img src = '${imageUrl}' alt = '${event.target.alt}'>`);
+
+    instance.show();
+
+    document.addEventListener("keydown", handleKeyPress);
+  }
+});
+
+function handleKeyPress(event) {
+  if (event.key === "Escape" && instance) {
+    instance.close();
+    document.removeEventListener("keydown", handleKeyPress);
+    instance = null;
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
