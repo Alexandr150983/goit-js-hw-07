4fcc944d4286a1929c2f27c7d57ea596083661a3
Alexandr150983 <alexandr150983@gmail.com>
2023-07-21 08:45:26 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function createGalleryItemMarkup({ preview, original, description }) {
+  return `
+  <li class="gallery__item">
+  <a class="gallery__link" href='${original}'>
+    <img
+      class="gallery__image"
+      src='${preview}'
+      data-source='${original}'
+      alt='${description}'
+    />
+  </a>
+</li>`;
+}
+
+function renderGallery() {
+  const galleryMarkup = galleryItems
+    .map((item) => createGalleryItemMarkup(item))
+    .join("");
+
+  galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+}
+
+renderGallery();
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt", 
+  captionsDelay: 250, 
+});
